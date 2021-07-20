ca7ef5c1a794dff5122703b88173fe6c44a22de5
JijoongHong <dshu1234@gmail.com>
2021-07-21 00:59:01 +0900

    Singleton

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -27,6 +27,18 @@ public class ConfigurationSingletonTest {
 
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        // @Configuration을 통해 싱글톤 보장
+        // CGLIB 바이코드 조작, appconfig 클래스를 상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록
+        // 오버라이드하여 등록되어있지 않으면 생성하여 빈에 등록, 있으면 컨테이너에서 찾아서 반환
+        // @Bean만 사용 시 컨테이너 등록은 되지만 싱글톤은 보장하지 않음
 
     }
 }
